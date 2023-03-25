4f653fae3915b139d422c34f79e3edc5b7907130
Vincenzo <vincenzo.delluniversit@virgilio.it>
2023-03-25 14:41:00 +0100

    Posting Data

--- app.js
+++ app.js
@@ -10,33 +10,12 @@ const db = mongoose.connect(uri)
   .catch((err) => debug(err));
 
 const app = express();
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
 
-bookRouter.route('/books')
-  .get((req, res) => {
-    const query = {};
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    });
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    });
-  });
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', bookRouter);
 
